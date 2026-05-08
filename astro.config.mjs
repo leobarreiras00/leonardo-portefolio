a51de0bf3a8ecc2453c25c2d65f5be5166b2d956
Leonardo Barreiras <202200130@estudantes.ips.pt>
2026-05-08 12:59:40 +0100

    feat: personalização completa do portefólio, dados e imagens

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import icon from 'astro-icon';
 
 // https://astro.build/config
@@ -13,5 +11,15 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [icon()]
+  integrations: [
+    icon({
+      include: {
+        "mdi": ["*"],
+        "skill-icons": ["*"],
+        "devicon": ["*"],
+        "devicon-plain": ["*"],
+        "vscode-icons": ["*"]
+      }
+    })
+  ]
 });
